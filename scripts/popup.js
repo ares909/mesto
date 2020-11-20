6d890479e0e3f59613d6dfa5f1693a114f843e6f
Камила Потеева <ares909@yandex.ru>
2020-11-20 11:27:24 +0300

    ready to deploy

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,10 +1,15 @@
 let editButton = document.querySelector('.profile__editbutton');
 let closeButton = document.querySelector('.popup__cross');
 let popup = document.querySelector('.popup');
+let nameInput = document.querySelector('#name');
+let professionInput = document.querySelector('#profession');
+let name = document.querySelector('.profile__name');
+let profession = document.querySelector('.profile__profession');
 
 function popupOpened() {
   popup.classList.add('popup_opened');
-  let nameInput = name.value
+  nameInput.value = name.textContent;
+  professionInput.value = profession.textContent;
 }
 
 editButton.addEventListener('click', popupOpened);
@@ -15,14 +20,12 @@ function popupClosed() {
 closeButton.addEventListener('click', popupClosed);
 
 let submitButton = document.querySelector('.popup__form-button');
-let name = document.querySelector('.profile__name');
-let profession = document.querySelector('.profile__profession');
+
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  let nameInput = document.querySelector('#name').value;
-  let professionInput = document.querySelector('#profession').value;
-
+  nameInput = document.querySelector('#name').value;
+  professionInput = document.querySelector('#profession').value;
   name.textContent = nameInput;
   profession.textContent = professionInput;
   popup.classList.remove('popup_opened');
@@ -30,3 +33,13 @@ function formSubmitHandler(evt) {
 }
 
 submitButton.addEventListener('click', formSubmitHandler);
+popup.addEventListener("keyup", function(evt) {
+
+  if (evt.keyCode === 13) {
+
+    evt.preventDefault();
+
+    document.querySelector('.popup__form-button').click();
+  }
+});
+
