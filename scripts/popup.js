4c03a0634a8c3cc1a85891a43821ee4949177775
Камила Потеева <ares909@yandex.ru>
2020-12-03 17:13:17 +0300

    5th sprint

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,30 +1,52 @@
 let editButton = document.querySelector('.profile__editbutton');
 let closeButton = document.querySelector('.popup__cross');
+let addButton = document.querySelector('.profile__addbutton');
 let popup = document.querySelector('.popup');
-let nameInput = document.querySelector('#name');
-let professionInput = document.querySelector('#profession');
+
 let name = document.querySelector('.profile__name');
 let profession = document.querySelector('.profile__profession');
 
-function popupOpened() {
+
+let formNameTemplate = document.querySelector('#form1');
+let formName = document.querySelector('#form1').content; //форма один убирается из template
+let nameInput = formName.querySelector('#name');
+let professionInput = formName.querySelector('#profession');
+
+
+let popupContainer = document.querySelector('#popup_container')
+let formTemplate = document.querySelector('#form2');
+let formPlace = document.querySelector('#form2').content;
+
+
+const popupOpened = () => {
+
   nameInput.value = name.textContent;
   professionInput.value = profession.textContent;
   popup.classList.add('popup_opened');
+  popupContainer.append(formName);
 }
 
-function popupClosed() {
+const popupClosed = () => {
   popup.classList.remove('popup_opened');
+
 }
 
-function formSubmitHandler(evt) {
+const formSubmitHandler = (evt) => {
   evt.preventDefault();
   name.textContent = nameInput.value;
   profession.textContent = professionInput.value;
   popupClosed();
 }
 
+const popupPlaceOpened = () => {
+  popup.classList.add('popup_opened');
+  popupContainer.append(formPlace);
+
+}
+
 closeButton.addEventListener('click', popupClosed);
 editButton.addEventListener('click', popupOpened);
+addButton.addEventListener('click', popupPlaceOpened);
 popup.addEventListener('submit', formSubmitHandler);
 
 
