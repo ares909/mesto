44bc67608f332eaf98a68860b12b421ae84ef943
Камила Потеева <ares909@yandex.ru>
2020-12-05 13:52:30 +0300

    2 form compelte

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,38 +5,37 @@ const popup = document.querySelector('.popup');
 const name = document.querySelector('.profile__name');
 const profession = document.querySelector('.profile__profession');
 const popupContainer = document.querySelector('#popup_container');
-const NameTemplate = document.querySelector('#popup_name').content;
-const PlaceTemplate = document.querySelector('#popup_place').content;
+const formName = document.querySelector('#form-name');
+const formPlace = document.querySelector('#form-place');
 const nameInput = document.querySelector('#name');
+const professionInput = document.querySelector('#profession');
 
 const popupOpened = () => {
   popup.classList.add('popup_opened');
-  const formName = NameTemplate.cloneNode(true);
-  formName.querySelector('#name').value = name.textContent;
-  formName.querySelector('#profession').value = profession.textContent;
-  popupContainer.append(formName);
-  }
+  formName.classList.add('popup__form_opened');
+  nameInput.value = name.textContent;
+  professionInput.value = profession.textContent;
+
+}
 
 const popupClosed = () => {
   popup.classList.remove('popup_opened');
   const popupForm = document.querySelectorAll('#form-name, #form-place');
   popupForm.forEach((item) => {
-    item.remove();
+    item.classList.remove('popup__form_opened');
   });
 }
 
 const popupPlaceOpened = () => {
   popup.classList.add('popup_opened');
-  const formPlace = PlaceTemplate.cloneNode(true);
-  popupContainer.append(formPlace);
+  formPlace.classList.add('popup__form_opened');
+
 }
 
 const formSubmitHandler = (evt) => {
   evt.preventDefault();
-  const nameItem = document.querySelector('#name').value;
-  name.textContent = nameItem;
-  const professionItem = document.querySelector('#profession').value;
-  profession.textContent = professionItem;
+  name.textContent = nameInput.value;
+  profession.textContent = professionInput.value;
   popupClosed();
 }
 
