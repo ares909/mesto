fa50d7076a8e06baef3c48ac880b30a5bc86840b
Камила Потеева <ares909@yandex.ru>
2020-12-07 21:56:51 +0300

    feat/ delete button/add button

--- scripts/popup.js
+++ scripts/popup.js
@@ -27,7 +27,6 @@ const initialCards = [
 
 const placeImage = document.querySelectorAll('.element__image');
 const placeName = document.querySelectorAll('.element__text');
-
 const editButton = document.querySelector('.profile__editbutton');
 const closeButton = document.querySelector('.popup__cross');
 const addButton = document.querySelector('.profile__addbutton');
@@ -37,25 +36,50 @@ const profession = document.querySelector('.profile__profession');
 const popupContainer = document.querySelector('#popup_container');
 const formName = document.querySelector('#form-name');
 const formPlace = document.querySelector('#form-place');
+const placeInput = document.querySelector('#place');
+const imageInput = document.querySelector('#image');
 const nameInput = document.querySelector('#name');
 const professionInput = document.querySelector('#profession');
 const elementContainer = document.querySelector('.elements');
 const elementTemplate = document.querySelector('#elementTemplate').content;
 
 
-const uploadCards = initialCards.map((item) => {
-  const elements = elementTemplate.cloneNode(true);
-  const elementImage = elements.querySelector('.element__image');
-  const elementText = elements.querySelector('.element__text');
-  elementImage.src = item.link;
-  elementImage.alt = item.name;
-  elementText.textContent = item.name;
-  return elements;
-});
-elementContainer.prepend(...uploadCards);
+//Шаблон для создания карточек
+const createCards = ({name, link}) => {
+  const element = elementTemplate.cloneNode(true);
+  const elementImage = element.querySelector('.element__image');
+  const elementText = element.querySelector('.element__text');
+  elementImage.src = link;
+  elementImage.alt = name;
+  elementText.textContent = name;
+  const likeButton = element.querySelectorAll('.element__like');
+//ставим лайе
+  likeButton.forEach((item) => {
+    item.addEventListener('click', (evt) => {
+      evt.target.classList.toggle('element__like_active');
+    });
+  });
+//удаляем карточку
+  const deleteButton = element.querySelector('.element__trash');
+  deleteButton.addEventListener('click', function (evt) {
+    const cardToRemove = evt.target.closest('#element');
+    cardToRemove.remove();
+  });
 
-const likeButton = document.querySelectorAll('.element__like');
+return element;
+}
 
+
+//Загрузка первоначальных карточек
+const uploadCards = () => {
+  const arrayCards = initialCards.map(createCards);
+  elementContainer.prepend(...arrayCards);
+}
+uploadCards();
+
+
+
+//открыть попап с именем
 const popupOpen = () => {
   popup.classList.add('popup_opened');
   formName.classList.add('popup__form_opened');
@@ -63,7 +87,7 @@ const popupOpen = () => {
   professionInput.value = profession.textContent;
 
 }
-
+//закрыть попап
 const popupClose = () => {
   popup.classList.remove('popup_opened');
   const popupForm = document.querySelectorAll('#form-name, #form-place');
@@ -71,12 +95,13 @@ const popupClose = () => {
     item.classList.remove('popup__form_opened');
   });
 }
-
+//открыть форму с местом
 const popupPlaceOpen = () => {
   popup.classList.add('popup_opened');
   formPlace.classList.add('popup__form_opened');
 }
 
+//сохранить форму с именем
 const formNameSubmitHandler = (evt) => {
   evt.preventDefault();
   name.textContent = nameInput.value;
@@ -84,11 +109,22 @@ const formNameSubmitHandler = (evt) => {
   popupClose();
 }
 
+
+//Залинкуй чтобы  в поля формы выводилась функция
+
+
+
+//Загрузка новой карточки
+
 const formPlaceSubmitHandler = (evt) => {
   evt.preventDefault();
-
+  const cardPlace = placeInput.value;
+  const cardImage = imageInput.value;
+  const element = createCards({name: cardPlace, link: cardImage});
+  elementContainer.prepend(element);
   popupClose();
 }
+
 
 
 //сделай второй сабмит и вторую кнопку с сабмитом
@@ -99,14 +135,16 @@ addButton.addEventListener('click', popupPlaceOpen);
 formName.addEventListener('submit', formNameSubmitHandler);
 formPlace.addEventListener('submit', formPlaceSubmitHandler);
 
-
 //ставим лайк
+
+/*const likeButton = document.querySelectorAll('.element__like');
 likeButton.forEach(function (item) {
   item.addEventListener('click', (evt) => {
 
-  evt.target.classList.toggle('element__like_active');
+    evt.target.classList.toggle('element__like_active');
   })
 })
+*/
 
 
 
