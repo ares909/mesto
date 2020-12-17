2ed4baf5e7bbb78dfde19c6e4b0758efefa0e949
Камила Потеева <ares909@yandex.ru>
2020-12-17 17:36:12 +0300

    6th sprint/ css-htnl done

---
+++ scripts/validate.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__form-text',
+  submitButtonSelector: '.popup__form-button',
+  inactiveButtonClass: 'popup__form-button_disabled',
+  inputErrorClass: 'popup__form_type_error',
+  errorClass: 'popup__error_visible'
+}); */
+
+
