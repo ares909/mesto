341446cc42527b037381e641cface52322c256c5
Камила Потеева <ares909@yandex.ru>
2021-01-22 22:05:00 +0300

    2nd review

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,15 +1,12 @@
-
-
 //даем исходную конфигурацию
 const validationSettings = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__form-text',
-  submitButtonSelector: '.popup__form-button',
-  inactiveButtonClass: 'popup__form-button_disabled',
-  inputErrorClass: 'popup__form_type_error',
-  errorClass: 'popup__error_visible'
+  formSelector: ".popup__form",
+  inputSelector: ".popup__form-text",
+  submitButtonSelector: ".popup__form-button",
+  inactiveButtonClass: "popup__form-button_disabled",
+  inputErrorClass: "popup__form_type_error",
+  errorClass: "popup__error_visible",
 };
-
 
 class FormValidator {
   constructor(settings, form) {
@@ -19,77 +16,70 @@ class FormValidator {
     this._submitButtonSelector = settings.submitButtonSelector;
     this._inactiveButtonClass = settings.inactiveButtonClass;
     this._inputErrorClass = settings.inputErrorClass;
-    this._errorClass = settings.errorClass
+    this._errorClass = settings.errorClass;
   }
+  //показать ошибку валидации
   _showError = (form, input) => {
     const error = form.querySelector(`#${input.id}-error`);
     input.classList.add(this._inputErrorClass);
     error.textContent = input.validationMessage;
     error.classList.add(this._errorClass);
-  }
+  };
   //скрываем ошибку валидации
   _hideError = (form, input) => {
     const error = form.querySelector(`#${input.id}-error`);
     input.classList.remove(this._inputErrorClass);
-    error.textContent = '';
+    error.textContent = "";
     error.classList.remove(this._errorClass);
-  }
+  };
   //условие валидации
   _checkInputValidity = (form, input) => {
     if (!input.validity.valid) {
       this._showError(form, input);
-    }
-    else {
+    } else {
       this._hideError(form, input);
     }
-  }
+  };
   //активируем/деактивируем кнопку
   _changeSubmitButton = (button, validation) => {
     if (!validation) {
       button.classList.add(this._inactiveButtonClass);
       button.disabled = true;
-    }
-    else {
+    } else {
       button.classList.remove(this._inactiveButtonClass);
       button.disabled = false;
     }
-  }
+  };
 
   //вешаем обработчики событий на все инпуты
   _setEventListeners = (form) => {
     const inputList = form.querySelectorAll(this._inputSelector);
     const submitButton = form.querySelector(this._submitButtonSelector);
     inputList.forEach((input) => {
-      //resetErrorMessage(form, input, settings);
-      input.addEventListener('input', () => {
+      input.addEventListener("input", () => {
         this._checkInputValidity(form, input);
-        this._changeSubmitButton(submitButton, form.checkValidity())
+        this._changeSubmitButton(submitButton, form.checkValidity());
       });
     });
-  }
+  };
   resetValidation(form) {
     const inputList = form.querySelectorAll(this._inputSelector);
     inputList.forEach((input) => {
-      this._hideError(form, input)
+      this._hideError(form, input);
     });
   }
   //вешаем обработчики событий на все формы
   enableValidation = () => {
     const formList = document.querySelectorAll(this._formSelector);
-    formList.forEach(form => {
+    formList.forEach((form) => {
       this._setEventListeners(form);
-      form.addEventListener('submit', (evt) => {
+      form.addEventListener("submit", (evt) => {
         evt.preventDefault();
       });
       const submitButton = form.querySelector(this._submitButtonSelector);
       this._changeSubmitButton(submitButton, form.checkValidity());
-    })
-  }
-
-  //убираем ошибку, если закрыли форму, не исправив ее
-
+    });
+  };
 }
 
 export { FormValidator, validationSettings };
-
-
--- scripts/index.js
+++ scripts/index.js
@@ -1,102 +1,115 @@
-
 //импортируем классы
-import Card from './Card.js';
-import {FormValidator, validationSettings} from './FormValidator.js'
-
+import Card from "./Card.js";
+import { FormValidator, validationSettings } from "./FormValidator.js";
 
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
-const editButton = document.querySelector('.profile__editbutton');
-const closeFormNameButton = document.querySelector('#close_card-name');
-const closeFormPlaceButton = document.querySelector('#close_card-place');
-const closeImageButton = document.querySelector('#close_image');
-const addButton = document.querySelector('.profile__addbutton');
-const popups = document.querySelectorAll('.popup');
-const name = document.querySelector('.profile__name');
-const profession = document.querySelector('.profile__profession');
-const formName = document.querySelector('#form-name');
-const formPlace = document.querySelector('#form-place');
-const placeInput = document.querySelector('#place-input');
-const imageInput = document.querySelector('#image-input');
-const nameInput = document.querySelector('#name-input');
-const professionInput = document.querySelector('#profession-input');
-const elementContainer = document.querySelector('.elements');
-const popupImage = document.querySelector('#popup_image-container');
+const editButton = document.querySelector(".profile__editbutton");
+const closeFormNameButton = document.querySelector("#close_card-name");
+const closeFormPlaceButton = document.querySelector("#close_card-place");
+const closeImageButton = document.querySelector("#close_image");
+const addButton = document.querySelector(".profile__addbutton");
+const popups = document.querySelectorAll(".popup");
+const name = document.querySelector(".profile__name");
+const profession = document.querySelector(".profile__profession");
+const formName = document.querySelector("#form-name");
+const formPlace = document.querySelector("#form-place");
+const placeInput = document.querySelector("#place-input");
+const imageInput = document.querySelector("#image-input");
+const nameInput = document.querySelector("#name-input");
+const professionInput = document.querySelector("#profession-input");
+const elementContainer = document.querySelector(".elements");
+const popupImage = document.querySelector("#popup_image-container");
 const formCard = document.forms.formCard;
+const formNameValidator = new FormValidator(validationSettings, formName);
+const formPlaceValidator = new FormValidator(validationSettings, formPlace);
 
 //открыть попап
 const openPopup = (popups) => {
-  popups.classList.add('popup_opened');
+  popups.classList.add("popup_opened");
   //вешаем на весь документ обработчик нажатия
-  document.addEventListener('keydown', closeByEscButton);
-}
+  document.addEventListener("keydown", closeByEscButton);
+};
 //закрыть попап
 const closePopup = (popups) => {
-  popups.classList.remove('popup_opened');
+  popups.classList.remove("popup_opened");
   //удаляем обработчик нажатия
-  document.removeEventListener('keydown', closeByEscButton);
-}
-
+  document.removeEventListener("keydown", closeByEscButton);
+};
+//открываем попап картинки
+const openImagePopup = () => {
+  openPopup(popupImage);
+};
 
 //создаем карточку через класс
 const addNewCard = (item) => {
-  const card = new Card(item, '#elementTemplate')._generateCard();
-  elementContainer.prepend(card)
-}
+  const card = new Card(
+    item,
+    "#elementTemplate",
+    openImagePopup
+  )._generateCard();
+  elementContainer.prepend(card);
+};
 //наполняем все карточки данными из массива и выводим
 initialCards.reverse().forEach((item) => {
   addNewCard(item);
- });
+});
 
 //Загрузка новой карточки
 const formPlaceSubmitHandler = (evt) => {
   evt.preventDefault();
   const name = placeInput.value;
   const link = imageInput.value;
-  const item = { name, link }
+  const item = { name, link };
   addNewCard(item);
   closeFormPlace();
-}
+};
 
 //открыть форму с именем
 const openFormName = () => {
   openPopup(formName);
   nameInput.value = name.textContent;
   professionInput.value = profession.textContent;
-  const formNameValidator = new FormValidator(validationSettings, formName).enableValidation();
-   //функция сброса ошибки
-  const validationReset = new FormValidator(validationSettings, formName).resetValidation(formName);
-}
+  formNameValidator.enableValidation();
+  //функция сброса ошибки
+  formNameValidator.resetValidation(formName);
+};
 
 //закрыть форму с именем
 const closeFormName = () => {
   closePopup(formName);
-}
+};
 
 //сохранить форму с именем
 const formNameSubmitHandler = (evt) => {
@@ -104,59 +117,48 @@ const formNameSubmitHandler = (evt) => {
   name.textContent = nameInput.value;
   profession.textContent = professionInput.value;
   closeFormName();
-}
-
+};
 
 //открыть форму с местом
 const openFormPlace = () => {
   openPopup(formPlace);
-  const formPlaceValidator = new FormValidator(validationSettings, formPlace).enableValidation();
-   //функция сброса ошибки
-  const validationReset = new FormValidator(validationSettings, formPlace).resetValidation(formPlace);
+  formPlaceValidator.enableValidation();
+  //функция сброса ошибки
+  formPlaceValidator.resetValidation(formPlace);
   formCard.reset();
-}
+};
 
 //закрыть форму с местом
 const closeFormPlace = () => {
   closePopup(formPlace);
-}
+};
 //закрыть попап с картинкой
 const closeImage = () => {
   closePopup(popupImage);
-}
-
+};
 
 //закрываем попапы кликом на оверлей
 popups.forEach((item) => {
-  item.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('popup')) {
+  item.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("popup")) {
       closePopup(evt.target);
     }
-  })
-})
-//закрываем формы нажатием Esc
+  });
+});
 
+//закрываем формы нажатием Esc
 function closeByEscButton(evt) {
-  if (evt.key === 'Escape') {
-    const popupOpened = document.querySelector('.popup_opened');
+  if (evt.key === "Escape") {
+    const popupOpened = document.querySelector(".popup_opened");
     closePopup(popupOpened);
   }
 }
 
-
 //Привязываем кнопки к функциям
-closeFormNameButton.addEventListener('click', closeFormName);
-closeFormPlaceButton.addEventListener('click', closeFormPlace);
-closeImageButton.addEventListener('click', closeImage);
-editButton.addEventListener('click', openFormName);
-addButton.addEventListener('click', openFormPlace);
-formName.addEventListener('submit', formNameSubmitHandler);
-formPlace.addEventListener('submit', formPlaceSubmitHandler);
-
-
-
-
-
-
-
-
+closeFormNameButton.addEventListener("click", closeFormName);
+closeFormPlaceButton.addEventListener("click", closeFormPlace);
+closeImageButton.addEventListener("click", closeImage);
+editButton.addEventListener("click", openFormName);
+addButton.addEventListener("click", openFormPlace);
+formName.addEventListener("submit", formNameSubmitHandler);
+formPlace.addEventListener("submit", formPlaceSubmitHandler);
